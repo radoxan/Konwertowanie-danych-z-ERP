b250c4e28ed5fb91a2ef0eb70d2dd438df3930c9
Radek Krupa <radekkrupa96@gmail.com>
2024-11-06 19:50:13 +0100

    dodano system wyceny

--- APP/pages/07_test_3.py
+++ APP/pages/07_test_3.py
@@ -1,44 +1,61 @@
-import pandas as pd
 import streamlit as st
 
-col1, col2, col3 = st.columns(3)
+def wycena_1(weight, bend_nums, price):
+    w = (weight*0.1)+((bend_nums-1.0)*(price-((bend_nums-1)*0.01)))
+    if w < 0:
+        w = 0
+    return w
+
+def wycena_2(bend_nums, price):
+    w = (price-((bend_nums-1)*0.01))*bend_nums
+    if w < 0:
+        w = 0
+    return w
+
+def wycena_3(weight, bend_nums, multi):
+    w = (weight*0.1)*(1.0+(multi*(bend_nums-1)))
+    if w < 0:
+        w = 0
+    return w
+
+def wycena_max(weight, bend_nums, price, multi):
+    w1 = wycena_1(weight, bend_nums, price)
+    w2 = wycena_2(bend_nums, price)
+    w3 = wycena_3(weight, bend_nums, multi)
+    w_max = max(w1,w2,w3)
+    return w_max
+
+col1, col2 = st.columns([3,1])
 with col1:
-    material = st.radio("Wybierz materiał:",['ocynk','czarna',])
+    weight = st.number_input("Podaj wagę",min_value=0.0,value=0.0,step=0.01,format="%0.2f")
 with col2:
-    thickness = st.radio("Wybierz grubość:",['1','2','3'])
-with col3:
-    length = st.radio("Wybierz materiał:",['0-500','501-1000',])
+    st.text(weight)
 
-data = [['ocynk', '1', '0-500', 'Tech_A'],
-        ['ocynk', '2', '0-500', 'Tech_B'],
-        ['ocynk', '3', '0-500', 'Tech_C'],
-        ['ocynk', '1', '501-1000', 'Tech_D'],
-        ['ocynk', '2', '501-1000', 'Tech_E'],
-        ['ocynk', '3', '501-1000', 'Tech_F'],
-        ['czarna', '1', '0-500', 'Tech_G'],
-        ['czarna', '2', '0-500', 'Tech_H'],
-        ['czarna', '3', '0-500', 'Tech_I'],
-        ['czarna', '1', '501-1000', 'Tech_J'],
-        ['czarna', '2', '501-1000', 'Tech_K'],
-        ['czarna', '3', '501-1000', 'Tech_L']]
+col1, col2 = st.columns([3,1])
+with col1:
+    bend_nums = st.number_input("Podaj ilość gięć",min_value=0,value=0,step=1,format="%d")
+with col2:
+    st.text(bend_nums)
 
-df_1 = pd.DataFrame(data, columns=['material','thickness','length','tech'])
+col1, col2 = st.columns([3,1])
+with col1:
+    price = st.number_input("Podaj minimalną cenę",min_value=0.0,value=0.0,step=0.01,format="%0.2f")
+with col2:
+    st.text(price)
 
-st.dataframe(df_1)
+col1, col2 = st.columns([3,1])
+with col1:
+    multi = st.number_input("Podaj współczynnik",min_value=0.0,value=0.0,step=0.01,format="%0.2f")
+with col2:
+    st.text(multi)
 
-def find_value(df, material, thickness, length):
-    # Filtrujemy DataFrame na podstawie podanych wartości
-    filtered_df = df[(df['material'] == material) & 
-                     (df['thickness'] == thickness) & 
-                     (df['length'] == length)]
-    
-    # Sprawdzamy, czy znaleziono odpowiedni wiersz
-    if not filtered_df.empty:
-        # Zwracamy wartość z ostatniej kolumny
-        return filtered_df.iloc[0, -1]
-    else:
-        return None
-    
-tech = find_value(df_1, material, thickness, length)
-st.write(tech)
+price_sum = wycena_max(weight, bend_nums, price, multi)
 
+st.title(f"Wycena elementu to: {price_sum:.5f}")
+
+price_1 = wycena_1(weight, bend_nums, price)
+st.header(f"Wycena 1: {price_1:.2f}")
+price_2 = wycena_2(bend_nums, price)
+st.header(f"Wycena 2: {price_2:.2f}")
+price_3 = wycena_3(weight, bend_nums, multi)
+st.header(f"Wycena 3: {price_3:.2f}")
